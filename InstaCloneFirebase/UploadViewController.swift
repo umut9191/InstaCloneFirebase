bf47875c5858604a8cafb410fac30b607ef7c09d
umut9191 <umutsurmeli91@gmail.com>
2022-01-09 22:01:08 +0300

    İmage Uploading screen designed and Image picker from device photo lib with gesture recognizer tabbing to image codes added to UploadViewController

--- InstaCloneFirebase/UploadViewController.swift
+++ InstaCloneFirebase/UploadViewController.swift
@@ -7,23 +7,36 @@
 
 import UIKit
 
-class UploadViewController: UIViewController {
+class UploadViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
 
+    @IBOutlet weak var uploadingImageView: UIImageView!
+    @IBOutlet weak var txtComment: UITextField!
+    @IBOutlet weak var btnUpload: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        uploadingImageView.isUserInteractionEnabled = true
+        
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(chooseImage))
         // Do any additional setup after loading the view.
+        uploadingImageView.addGestureRecognizer(gestureRecognizer)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @objc func chooseImage() {
+        //reaching user fotos lib;
+        let pickerController = UIImagePickerController()
+        pickerController.delegate = self
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
+        
     }
-    */
+    //picker method;
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        uploadingImageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    @IBAction func uploadButtonPressed(_ sender: UIButton) {
+    }
+    
 
 }
